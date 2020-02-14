655666d16dadcbdee3cf59be4bc2e93d65e50a20
IKE0825 <itoken0825@gmail.com>
2020-02-14 17:25:10 +0900

    コメント追加

--- predict-image.py
+++ predict-image.py
@@ -2,21 +2,26 @@ import sklearn.datasets
 import sklearn.svm
 import PIL.Image
 import numpy
+import pdb
 
 def imageToData(filename):
-    grayImage = PIL.Image.open(filename).convert("L")
-    grayImage = grayImage.resize((8,8),PIL.Image.ANTIALIAS)
+    grayImage = PIL.Image.open(filename).convert("L") #Imageモード　L : 8bitグレイスケール
+    grayImage = grayImage.resize((8,8),PIL.Image.ANTIALIAS) #画像サイズの縮小、アンチエアリアス処理
     numImage = numpy.asarray(grayImage,dtype = float)
-    numImage = numpy.floor(16 - 16 * (numImage / 256))
-    numImage = numImage.flatten()
+    print(numImage)
+    numImage = numpy.floor(16 - 16 * (numImage / 256)) #floor 切り捨て
+    print(numImage)
+    numImage = numImage.flatten() # リストの平坦化
+    print(numImage)
     return numImage
 
 def predictDigits(data):
     digits = sklearn.datasets.load_digits()
-    clf = sklearn.svm.SVC(gamma = 0.001)
-    clf.fit(digits.data,digits.target)
+    clf = sklearn.svm.SVC(gamma = 0.001) #機械学習用マシン
+    clf.fit(digits.data,digits.target) 
     n = clf.predict([data])
     print("予測は= " , n)
 
-data = imageToData("2.png")
+pdb.set_trace()
+data = imageToData(r"C:\Users\ec000248\Documents\Git\200222_training\2.png")
 predictDigits(data)
